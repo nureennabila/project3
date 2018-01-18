d1477de1d5412678e7dc2917586cd9c1954c0ae5
ICTKVPJB <ICTKVPJB@ICTKVPJB-PC>
2018-01-18 10:54:52 +0800

    latihan2

---
+++ src/kpdnureen/markah.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kpdnureen;
+
+/**
+ *
+ * @author ICTKVPJB
+ */
+
+    class Markah 
+{
+  public static void main(String args[])
+  {
+
+   int bm=78;
+   int bi=53;
+   int mt=81;
+   int sj=97;
+
+
+int jumlah= bm+bi+mt+sj;
+int purata= jumlah/4;
+int peratus= 309*100/400;
+
+    System.out.println( "jumlah keseluruhan:"+jumlah);
+    System.out.println( "purata keseluruhan:" +purata);
+    System.out.println( "Jumlah peratusan:"+ peratus);
+    }
+    }
+
+    
